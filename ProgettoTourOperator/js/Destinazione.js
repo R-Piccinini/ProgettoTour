e6a1ed2e11b9109c9a5bdbc51a872b2b5f6f3269
fede5599 <federicazaccaro@outlook.it>
2024-07-19 17:37:06 +0200

    jsdimer

--- ProgettoTourOperator/js/Destinazione.js
+++ ProgettoTourOperator/js/Destinazione.js
@@ -1,29 +1,45 @@
+// Funzione per ottenere gli elementi dal server
+document.addEventListener('DOMContentLoaded', () => {
+const titolo = document.getElementById('titoloPag');
+const descrizione = document.getElementById('descrizionedest');
+const imgcarosello = document.getElementById('imgcarol1');
+const imgcarosello2 = document.getElementById('imgcarol2');
+const imgcarosello3 = document.getElementById('imgcarol3');
+const nomecarosello1 = document.getElementById('nomedest1');
+const nomecarosello2 = document.getElementById('nomedest2');
+const nomecarosello3 = document.getElementById('nomedest3');
 
-const titolo=document.getElementById('titoloPag');
-const descrizione=document.getElementById('descrizionedest');
-const imgcarosello=document.getElementById('imgcarol1');
-const imgcarosello2=document.getElementById('imgcarol2');
-const imgcarosello3=document.getElementById('imgcarol3');
-const nomecarosello1=document.getElementById('nomedest1');
-const nomecarosello2=document.getElementById('nomedest2');
-const nomecarosello3=document.getElementById('nomedest3');
+const europa = document.getElementById('destinazione1');
+const africa = document.getElementById('destinazione2');
+const america = document.getElementById('destinazione3');
+const asia = document.getElementById('destinazione1');
+const oceania = document.getElementById('destinazione2');
+const stelline = document.getElementById('valutazione');
 
+const immagine=document.getElementById('imgpacchetto');
 
-document.addEventListener('DOMContentLoaded', function loadDest() {
     const urlParams = new URLSearchParams(window.location.search);
     const id = urlParams.get('id');
     const url = `http://localhost:8080/destinazione/${id}`;
     fetch(url)
-      .then(res => res.json())
-      .then(dest => {
-        console.log(dest)
-        titolo.innerHTML=dest.name;
-        descrizione.innerHTML=dest.descrizione;
-        if(dest.id==1){
-            imgcarosello.src=`url(assets/Destinazioni/canada-1.png)`;
+        .then(res => res.json())
+        .then(pack => {
+            console.log(pack)
+            europa.innerHTML = "Europa" + pack.europa;
 
-        }
+
+
+
+
+
+            titolo.innerHTML = pack.name;
+            descrizione.innerHTML = "bhoo";
+            if (pack.id == 1) {
+                imgcarosello.src = `url(assets/Destinazioni/canada-1.png)`;
+            }
+            if (pacchetto1.innerHTML)
+
 
     })
-    .catch(error => console.error('Error:', error));
- }, false);
+        .catch(error => console.error('Error:', error));
+}, false);
