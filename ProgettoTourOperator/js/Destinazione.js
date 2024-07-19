dd0019753dcbd95103ffece1f2045da33b575cac
R-Piccinini <riccardo.piccinini.21@gmail.com>
2024-07-19 21:57:02 +0200

    help

--- ProgettoTourOperator/js/Destinazione.js
+++ ProgettoTourOperator/js/Destinazione.js
@@ -1,10 +1,11 @@
 
 const titolo=document.getElementById('titoloPag');
+const nome=document.getElementById('nomePag')
 const descrizione=document.getElementById('descrizionedest');
 const imgcarosello=document.getElementById('imgcarol1');
 const imgcarosello2=document.getElementById('imgcarol2');
 const imgcarosello3=document.getElementById('imgcarol3');
-const nomecarosello1=document.getElementById('nomedest1');
+const nomecarosello=document.getElementById('nomedest1');
 const nomecarosello2=document.getElementById('nomedest2');
 const nomecarosello3=document.getElementById('nomedest3');
 
@@ -15,13 +16,14 @@ document.addEventListener('DOMContentLoaded', function loadDest() {
     const url = `http://localhost:8080/destinazione/${id}`;
     fetch(url)
       .then(res => res.json())
-      .then(pack => {
-        console.log(pack)
-        titolo.innerHTML=pack.name;
-        descrizione.innerHTML=pack.descrizione;
-        if(pack.id==1){
-            imgcarosello.src=`url(assets/Destinazioni/canada-1.png)`;
-
+      .then(dest => {
+        console.log(dest)
+        titolo.innerHTML=dest.name;
+        nome.innerHTML=dest.name
+        descrizione.innerHTML=dest.descrizione;
+        if(dest.id==1){
+            imgcarosello.src="assets/Destinazioni/centralpark.png";
+            nomecarosello.innerHTML="HELP"
         }
 
     })
