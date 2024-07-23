806cd9ffd301346d1e7b00bb2909eede7cf853a4
R-Piccinini <riccardo.piccinini.21@gmail.com>
2024-07-23 02:09:25 +0200

    gestore finito

--- ProgettoTourOperator/js/Gestore.js
+++ ProgettoTourOperator/js/Gestore.js
@@ -1,5 +1,23 @@
 const tableUser = document.getElementById('tableUser');
 const tablePack = document.getElementById('tablePack');
+const editUsernameUser = document.getElementById('editUsernameUser')
+const editPasswordUser = document.getElementById('editPasswordUser')
+const editEmailUser = document.getElementById('editEmailUser')
+const editNomeUser = document.getElementById('editNomeUser')
+const editCognomeUser = document.getElementById('editCognomeUser')
+const editIndirizzoUser = document.getElementById('editIndirizzoUser')
+const editDataUser = document.getElementById('editDataUser')
+const editPacchettoUser = document.getElementById('editPacchettoUser')
+const idUser = document.getElementById('idUser')
+const idPack=document.getElementById('idPack')
+const editNamePack=document.getElementById('editNamePack')
+const editPrezzoPack=document.getElementById('editPrezzoPack')
+const editDescrizionePack=document.getElementById('editDescrizionePack')
+const editSistemazionePack=document.getElementById('editSistemazionePack')
+const editDurataPack=document.getElementById('editDurataPack')
+const editImgPack=document.getElementById('editImgPack')
+
+
 
 function loadUser() {
     const url = "http://localhost:8080/user";
@@ -23,7 +41,7 @@ function loadUser() {
                     <td> <button class="btn me-md-2 mb-3" type="button"
                             style="--bs-btn-padding-y: .10rem; --bs-btn-padding-x: .3rem;" onclick='deleteUser(${utente.id})'>Elimina</button></td>
                     <td><button class="btn me-md-2 mb-3" id="btnmodifiche" data-bs-toggle="collapse"
-                            href="#tabellamodificauser" type="button"
+                            href="#tabellamodificauser" type="button" onclick='loadSingleUser(${utente.id})'
                             style="--bs-btn-padding-y: .10rem; --bs-btn-padding-x: .3rem;">Modifica</button></td>
                             `
                 tableUser.appendChild(tr)
@@ -46,12 +64,63 @@ function deleteUser(id) {
 
 };
 
-function editUser() {
+function loadSingleUser(id) {
+    const url = `http://localhost:8080/user/${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(utente => {
+            idUser.value = utente.id
+            editUsernameUser.value = utente.username
+            editPasswordUser.value = utente.password
+            editEmailUser.value = utente.email
+            editNomeUser.value = utente.nome
+            editCognomeUser.value = utente.cognome
+            editIndirizzoUser.value = utente.indirizzo
+            editDataUser.value = utente.dataNascita
 
+        })
 }
 
+function clearUser() {
+    idUser.value = ''
+    editUsernameUser.value = ''
+    editPasswordUser.value = ''
+    editEmailUser.value = ''
+    editNomeUser.value = ''
+    editCognomeUser.value = ''
+    editIndirizzoUser.value = ''
+    editDataUser.value = ''
+}
 
-function loadPack(){
+function editUser() {
+    const id = idUser.value;
+    const method = id ? 'PUT' : 'POST';
+    const url = id ? `http://localhost:8080/user/${id}` : 'http://localhost:8080/user';
+    const user = {
+        id: id,
+        username: editUsernameUser.value,
+        password: editPasswordUser.value,
+        email: editEmailUser.value,
+        nome: editNomeUser.value,
+        cognome: editCognomeUser.value,
+        indirizzo:editIndirizzoUser.value,
+        dataNascita:editDataUser.value
+    };
+
+    fetch(url, {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    }).then(() => {
+        loadUser()
+    });
+};
+
+
+
+function loadPack() {
     const url = "http://localhost:8080/pacchetto";
     fetch(url)
         .then(res => res.json())
@@ -71,7 +140,7 @@ function loadPack(){
                     <td> <button class="btn me-md-2 mb-3" type="button"
                             style="--bs-btn-padding-y: .10rem; --bs-btn-padding-x: .3rem;" onclick='deletePack(${pack.id})'>Elimina</button></td>
                     <td><button class="btn me-md-2 mb-3" id="btnmodifiche" data-bs-toggle="collapse"
-                            href="#tabellamodificapacchetto" type="button"
+                            href="#tabellamodificapacchetto" type="button" onclick='loadSinglePack(${pack.id})'
                             style="--bs-btn-padding-y: .10rem; --bs-btn-padding-x: .3rem;">Modifica</button></td>
                             `
                 tablePack.appendChild(tr)
@@ -79,6 +148,9 @@ function loadPack(){
         })
         .catch(error => console.error('Error:', error));
 }
+
+
+
 
 function deletePack(id) {
     if (confirm('Sei sicuro di volere elimiare?')) {
@@ -91,4 +163,54 @@ function deletePack(id) {
 
     }
 
+};
+
+function loadSinglePack(id) {
+    const url = `http://localhost:8080/pacchetto/${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(pack => {
+            idPack.value = pack.id
+            editNamePack.value = pack.name
+            editPrezzoPack.value = pack.price
+            editDescrizionePack.value = pack.descrizione
+            editSistemazionePack.value = pack.sistemazione
+            editDurataPack.value = pack.durata
+            editImgPack.value = pack.immagine
+        })
+}
+
+function clearPack() {
+    idPack.value = ''
+    editNamePack.value = ''
+    editPrezzoPack.value = ''
+    editDescrizionePack.value = ''
+    editSistemazionePack.value = ''
+    editDurataPack.value = ''
+    editImgPack.value = ''
+}
+
+function editPack() {
+    const id = idPack.value;
+    const method = id ? 'PUT' : 'POST';
+    const url = id ? `http://localhost:8080/pacchetto/${id}` : 'http://localhost:8080/pacchetto';
+    const pack = {
+        id: id,
+        name: editNamePack.value,
+        price: editPrezzoPack.value,
+        descrizione: editDescrizionePack.value,
+        sistemazione: editSistemazionePack.value,
+        durata: editDurataPack.value,
+        immagine:editImgPack.value,
+    };
+
+    fetch(url, {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(pack)
+    }).then(() => {
+        loadPack()
+    });
 };
